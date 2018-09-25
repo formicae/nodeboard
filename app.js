f3da015cd3ed536b96ec10c74e163fd865818872
formicae <charly0102@naver.com>
2018-09-25 18:45:54 +0900

    # app.js :     use static for /images
    # index.js :
        post method router of /user/upload is added for image upload.
    
    # user.ejs :
        image upload logic updated
        - img tag for image has been implemented.
        - Form for file upload has been implemented.
        - AJAX has been implemented to render uploaded image in html.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ connect();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/routes'));
+app.use(express.static(__dirname + '/images'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
